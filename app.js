7fd1a39fbaad77205028848eea9ea3707656810b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-29 10:27:59 -0800

    Start server to see if it is connected to port 3000 and connected to DB

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+// Database
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('connected to DB!'))
+.catch((err) => console.log(err))
+
+
+
+
+// Start Server
+app.listen(3000, () => console.log('Server started on port 3000...'))
