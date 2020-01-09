5c6cc12dcd393ea107bbed3830ebea16f94cbdfc
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-08 22:37:05 -0700

    make it better

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Database
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('connected to DB!'))
-.catch((err) => console.log(err))
+// mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// .then(() => console.log('connected to DB!'))
+// .catch((err) => console.log(err))
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true } || 'mongodb://localhost/plant-shop-api');
 
 // Middleware
 app.use(express.json());
@@ -21,4 +23,7 @@ app.get('/:id', plantsRoute.findAPlant);
 app.patch('/:id', plantsRoute.updatePlant);
 
 // Start Server
-app.listen(3000, () => console.log('Server started on port 3000...'))
+// app.listen(3000, () => console.log('Server started on port 3000...'))
+
+const port = process.env.PORT || 3000;
+app.listen(port);
