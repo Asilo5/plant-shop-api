c8cfc2edcc79f97d7ba30dab81d8899dfdba6d6f
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-08 23:08:04 -0700

    untracked node_modules

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Database
-// mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-// .then(() => console.log('connected to DB!'))
-// .catch((err) => console.log(err))
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('connected to DB!'))
+.catch((err) => console.log(err))
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true } || 'mongodb://localhost/plant-shop-api');
+// mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true } || 'mongodb://localhost/plant-shop-api');
 
 // Middleware
 app.use(express.json());
@@ -26,4 +26,4 @@ app.patch('/:id', plantsRoute.updatePlant);
 // app.listen(3000, () => console.log('Server started on port 3000...'))
 
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port, console.log("MongoDB is Connected..."));
