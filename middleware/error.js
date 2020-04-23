9d250cd0f1a192c4ee9f5df3ebb701c8318f4f6c
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-23 13:12:28 -0600

    Implement asynchandler package to replace the try catch blocks

--- middleware/error.js
+++ middleware/error.js
@@ -3,7 +3,7 @@
 const errorHandler = (err, req, res, next) =>{
     // check the error type for more information on the response
     console.log(err);
-    // mongoose Error => CastError 
+    // mongoose Error => CastError
     if(err.name === 'CastError'){
         err = new Error(`Resource id: ${err.value} is invalid`);
     }
