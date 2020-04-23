c0be184d08691ce0b240e638144106e0d1ce1a2f
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-23 12:45:18 -0600

    Add cast error handling on the error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,15 @@
 
 
 const errorHandler = (err, req, res, next) =>{
+    // check the error type for more information on the response
+    console.log(err);
+    // mongoose Error => CastError 
+    if(err.name === 'CastError'){
+        err = new Error(`Resource id: ${err.value} is invalid`);
+    }
+    
 
-    res.status(500).json({
+    res.status(err.status || 500 ).json({
         success: false,
         error: err.message || 'Server Error'
     })
