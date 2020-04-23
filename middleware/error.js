52964527ccbf0a4990482dcef9e311fc5beb184e
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-04-23 11:34:27 -0600

    Add the express error handling middleware

---
+++ middleware/error.js
@@ -0,0 +1,11 @@
+
+
+const errorHandler = (err, req, res, next) =>{
+
+    res.status(500).json({
+        success: false,
+        error: err.message || 'Server Error'
+    })
+  }
+
+  module.exports = errorHandler;
